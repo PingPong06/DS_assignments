69f0a8e7a3acfddff909f166dc1f6879e5f748d3
Darshan_Wagh <darshanwaghwce@gmail.com>
2025-10-19 10:32:15 +0530

    Stack 2 added

---
+++ stack_2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+// # define n 100
+class stack{
+
+public:
+    int top;
+    char *arr;
+    int size;
+    stack(int size){
+        this->size = size;
+        arr = new char[size];
+        top = -1;
+    }
+    void push(char data){
+        if(top >= size-1){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        arr[++top]=data;
+    }
+
+    void pop(){
+        if(top == -1){
+            cout<<"Stack Underflow"<<endl;
+            return ;
+        }
+        cout<<arr[top--]<<" ";
+    }
+};
+int main(){
+    int size;
+    cout<<"Enter the size of the string: ";
+    cin>>size; 
+    cin.ignore();
+    stack obj(size);
+    string reverseString;
+    getline(cin , reverseString);
+    for(int i=0;i<reverseString.size();i++){
+        obj.push(reverseString[i]);
+    }
+    for(int i=0;i<reverseString.size();i++){
+        obj.pop();
+    }
+    return 0;
+}
